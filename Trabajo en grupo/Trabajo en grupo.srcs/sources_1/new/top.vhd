439e78717cf32c9fc9090c7ac4085f00a3638144
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2022-12-04 18:37:35 +0100

    Esqueleto entidad top
    Declaradas las entradas y las salidas de la entidad top

--- Trabajo en grupo/Trabajo en grupo.srcs/sources_1/new/top.vhd
+++ Trabajo en grupo/Trabajo en grupo.srcs/sources_1/new/top.vhd
@@ -1,38 +1,24 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 27.11.2022 16:03:59
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.numeric_std.all;
 
 entity top is
---  Port ( );
+    generic(
+        button_width    : positive := 4;      -- Numero de botones
+        speed_width     : positive := 4;      -- Bits para la comunicacion con el motor
+        incl_width      : positive := 4;      -- Bits para la comunicacion con el motor
+        digits          : positive := 8      -- Numero de displays de 7 segmentos de la placa
+    );
+    port(
+        BUTTONS     : in std_logic_vector(button_width-1 downto 0);     --Botones de la placa
+        RESET       : in std_logic;                                     -- RESET de todo el sistema, asincrono
+        CLK         : in std_logic;                                     -- Reloj
+        START       : in std_logic;                                     -- Para que la cinta comience a funcionar  
+        SPEED       : out std_logic_vector (speed_width - 1 downto 0);  -- Al control del motor de velocidad
+        INCL        : out std_logic_vector (incl_width - 1 downto 0);   -- Al control del motor de inclinacion
+        BCD_DATA    : out std_logic_vector (7 downto 0);                -- Salida datos a los displays
+        BDC_SEL     : out std_logic_vector (digits - 1 downto 0)        -- Seleccion del digito
+    );
 end top;
 
 architecture Behavioral of top is
