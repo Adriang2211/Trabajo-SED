51a9a297f56e9f4505e235f6091c5617c8e1b05d
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2022-12-05 11:52:39 +0100

    Creación del divisor de frecuencia
    Creado divisor de frecuencia (entidad y arquitectura)

---
+++ Trabajo en grupo/Trabajo en grupo.srcs/sources_1/new/FREQ_DIV.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity FREQ_DIV is
+    Generic(
+        DIVIDE  : positive := 10
+    );
+    Port ( 
+        CLK_IN : in STD_LOGIC;
+        CLK_OUT : out STD_LOGIC
+        );
+end FREQ_DIV;
+
+architecture Behavioral of FREQ_DIV is
+    signal counter : integer := 0;
+begin
+    process (CLK_IN)
+    begin
+        CLK_OUT <= '0';
+        if (rising_edge(CLK_IN)) then
+            counter <= counter +1;
+            if (counter = DIVIDE) then
+                counter <= 0;
+                CLK_OUT <= '1';
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
