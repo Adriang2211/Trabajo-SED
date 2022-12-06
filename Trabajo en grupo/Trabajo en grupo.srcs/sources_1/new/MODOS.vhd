8453c4ddb7168447c729921fe37202a319826bb5
laura-alvrz <l.alvarezp@alumnos.upm.es>
2022-12-06 16:16:53 +0100

    Añadida estructura en MODOS
    Se ha añadido parte de la estructura de MODOS así como un testbench sencillo para comprobar su funcionamiento

--- Trabajo en grupo/Trabajo en grupo.srcs/sources_1/new/MODOS.vhd
+++ Trabajo en grupo/Trabajo en grupo.srcs/sources_1/new/MODOS.vhd
@@ -16,16 +16,60 @@ entity MODOS is
         CLK        : in  STD_LOGIC; -- Señal de reloj
         RESET      : in  STD_LOGIC; -- Reset asincrono
         ENABLE     : in  STD_LOGIC; -- Chip enable
-        SPEED      : out STD_LOGIC; -- Al control del motor de velocidad (LED)
-        INCL       : out STD_LOGIC; -- Al control del motor de inclinacion (LED)
+        SPEED      : out UNSIGNED (speed_width-1 downto 0); -- Al control del motor de velocidad (LED)
+        INCL       : out UNSIGNED (incl_width-1 downto 0); -- Al control del motor de inclinacion (LED)
         SPEED_DATA : out UNSIGNED (speed_width-1 downto 0); -- Dato para mostrar en pantalla (display)
         INCL_DATA  : out UNSIGNED (incl_width-1 downto 0) -- Dato para mostrar en pantalla (display)
     );
 end MODOS;
 
-architecture Behavioral of MODOS is
+architecture Structural of MODOS is
+    component LEVEL_CTRL is
+        Generic(
+            lv_width : positive := 4
+        );
+    
+        Port ( 
+            UP         : in  STD_LOGIC; 
+            DOWN       : in  STD_LOGIC; 
+            CLK        : in  STD_LOGIC; 
+            RESET      : in  STD_LOGIC; 
+            CE         : in  STD_LOGIC; 
+            LEVEL      : out UNSIGNED (lv_width-1 downto 0)
+        );
+    end component LEVEL_CTRL;
+    
 
 begin
-
-
-end Behavioral;
+    spd : if SPEED_UP = '1' or SPEED_DOWN = '1' generate
+    inst_S : LEVEL_CTRL
+    generic map (
+        lv_width => speed_width
+    )
+    port map (
+        UP => SPEED_UP,
+        DOWN => SPEED_DOWN,
+        CLK => CLK,
+        RESET => RESET,
+        CE => ENABLE,
+        LEVEL => SPEED
+    );
+    end generate spd;
+    
+    inc : if INCL_UP = '1' or INCL_DOWN = '1' generate
+    inst_I : LEVEL_CTRL
+    generic map (
+        lv_width => incl_width
+    )
+    port map (
+        UP => INCL_UP,
+        DOWN => INCL_DOWN,
+        CLK => CLK,
+        RESET => RESET,
+        CE => ENABLE,
+        LEVEL => INCL
+    );
+    end generate inc;
+    
+    
+end Structural;
