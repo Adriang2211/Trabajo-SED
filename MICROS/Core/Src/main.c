17051be93240e1144edc6ed320c44ccd9e77fed2
laura-alvrz <l.alvarezp@alumnos.upm.es>
2023-01-04 13:47:36 +0100

    Ajuste del temporizador

--- MICROS/Core/Src/main.c
+++ MICROS/Core/Src/main.c
@@ -459,7 +459,7 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_ACTIVE;
-  sConfigOC.Pulse = 9999;
+  sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
